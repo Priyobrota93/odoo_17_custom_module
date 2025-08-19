4230fe494abff7a401f13a0ed5dcde8275b7e4b5
Priyobrota <luiz.khim962gmail.com>
2025-08-19 14:51:13 +0600

    Cleaned up extra code in the patient field

--- custom_addons/bancat_management_system/models/patient.py
+++ custom_addons/bancat_management_system/models/patient.py
@@ -69,9 +69,6 @@ class Patient(models.Model):
     latest_folder_id = fields.Many2one('documents.folder', string="Latest Document Folder",
                                        compute="_compute_latest_folder_id", store=True)
     document_count = fields.Integer(compute="_compute_document_count", string="Document Count")
-    # document_count = fields.Integer(compute="_compute_document_count",
-    #                                               string="Latest Folder Documents")
-
 
     last_visit_state = fields.Selection([
         ('check_in', 'Check In'),
@@ -99,13 +96,6 @@ class Patient(models.Model):
     previous_treatment_details = fields.Text(string="Previous Treatment Details", tracking=False)
     previous_bed_allocation_id = fields.Many2one('bed.allocation', string="Previous Bed Allocation", tracking=False)
 
-    # Flag to track if we need to hide some fields from the UI
-    # is_active_visit = fields.Boolean(string="Is Active Visit", compute="_compute_is_active_visit", store=True)
-
-    # @api.depends('state')
-    # def _compute_is_active_visit(self):
-    #     for patient in self:
-    #         patient.is_active_visit = patient.state == 'check_in'
 
     @api.depends('visit_ids.state', 'visit_ids.start_date')
     def _compute_last_visit_state(self):
@@ -153,27 +143,11 @@ class Patient(models.Model):
             'patient_id': patient.id,
             'state': 'check_in',
             'start_date': fields.Datetime.now(),
-            # 'cancer_type': patient.cancer_type.id if patient.cancer_type else False,
-            # 'cancer_stage': patient.cancer_stage,
-            # 'current_hospital': patient.current_hospital.id if patient.current_hospital else False,
-            # 'treatment_details': patient.treatment_details,
-            # 'current_status': patient.current_status,
-            # 'approximate_amount': patient.approximate_amount,
-
         })
 
         # Mark the bed as unavailable after allocation
         if patient.bed_allocation_id:
             patient.bed_allocation_id.is_available = False
-
-        # Documents module with the format 'name_patient_id' and Assign the patient's documents
-        # folder_name = f"{patient.name}_{patient.patient_id}"
-        # folder = self.env['documents.folder'].create({
-        #     'name': folder_name,
-        #     'description': f"Folder for patient {patient.name} with ID {patient.patient_id}",
-        # })
-        #
-        # patient.document_ids.write({'folder_id': folder.id})
 
         return patient
 
@@ -192,10 +166,6 @@ class Patient(models.Model):
             else:
                 patient.latest_folder_id = False
 
-    # @api.depends('document_ids')
-    # def _compute_document_count(self):
-    #     for patient in self:
-    #         patient.document_count = len(patient.document_ids)
 
     @api.depends('latest_folder_id')
     def _compute_document_count(self):
@@ -307,9 +277,7 @@ class Patient(models.Model):
             'end_date': False,
         })
 
-        # If bed is allocated, mark it as unavailable
-        # if self.bed_allocation_id:
-        #     self.bed_allocation_id.is_available = False
+
 
         # If there are previous values stored, restore them
         if self.previous_cancer_type_id or self.previous_cancer_stage or self.previous_current_hospital_id or self.previous_treatment_details or self.previous_current_status:
@@ -327,11 +295,6 @@ class Patient(models.Model):
                 'previous_current_status': False,
             })
 
-        # If there's a previous bed allocation, try to allocate it if available
-        # if self.previous_bed_allocation_id and self.previous_bed_allocation_id.is_available:
-        #     self.bed_allocation_id = self.previous_bed_allocation_id.id
-        #     self.previous_bed_allocation_id.is_available = False
-        #     self.previous_bed_allocation_id = False
 
         # Refresh the latest folder
         self._compute_latest_folder_id()
@@ -344,47 +307,6 @@ class Patient(models.Model):
         self.ensure_one()
         now = fields.Datetime.now()
 
-        # Store the current bed allocation before clearing
-        # old_bed = self.bed_allocation_id
-
-        # Clear fields and mark bed as available
-        # values = {
-        #     'state': 'check_out',
-        #     'end_date': now,
-        #     # Clear the specified fields
-        #     'cancer_type': False,
-        #     'cancer_stage': False,
-        #     'current_hospital': False,
-        #     'treatment_details': False,
-        #     'current_status': False,
-        #     'bed_allocation_id': False,
-        #     'approximate_amount': 0.0,
-        # }
-
-        # current_visit = self.visit_ids.filtered(lambda v: v.state == 'check_in')
-        # if current_visit:
-        #     current_visit[-1].write({
-        #         'state': 'check_out',
-        #         'end_date': now,
-        #         'cancer_type': self.cancer_type.id if self.cancer_type else False,
-        #         'cancer_stage': self.cancer_stage,
-        #         'current_hospital': self.current_hospital.id if self.current_hospital else False,
-        #         'treatment_details': self.treatment_details,
-        #         'current_status': self.current_status,
-        #         'approximate_amount': self.approximate_amount,
-        #     })
-        #
-        # # Make the bed available again if there was one allocated
-        # if old_bed:
-        #     old_bed.is_available = True
-        #
-        # # Archive existing attendants (we keep the history but they won't show in the form)
-        # if self.attendance_ids:
-        #     for attendant in self.attendance_ids:
-        #         attendant.is_latest = False
-
-
-
         self.write({
             'state': 'check_out',
             'end_date': now,
@@ -396,14 +318,6 @@ class Patient(models.Model):
             'previous_current_status': self.current_status,
             'previous_bed_allocation_id': self.bed_allocation_id.id if self.bed_allocation_id else False,
         })
-
-
-        # open_visits = self.visit_ids.filtered(lambda v: v.state == 'check_in')
-        # if open_visits:
-        #     open_visits[-1].write({
-        #         'state': 'check_out',
-        #         'end_date': now,
-        #     })
 
         # Create a final record in patient_visit with all data before clearing
         if self.visit_ids and any(v.state == 'check_in' for v in self.visit_ids):
@@ -457,130 +371,3 @@ class Patient(models.Model):
             'name': 'Release Paper',
             'context': {'lang': self.env.context.get('lang')},
         }
-
-    # def print_release_paper(self):
-    #     if not self:
-    #         raise UserError("No record selected.")
-    #     return self.env.ref('bancat_management_system.action_release_paper_report').report_action(self)
-
-    # @api.depends('document_ids')
-    # def _compute_document_count(self):
-    #     for record in self:
-    #         record.document_count = len(record.document_ids)
-
-    # def action_open_documents(self):
-    #     self.ensure_one()
-    #     return {
-    #         'type': 'ir.actions.act_window',
-    #         'name': 'Patient Documents',
-    #         'res_model': 'documents.document',
-    #         'view_mode': 'tree,form',
-    #         'domain': [('patient_id', '=', self.id)],
-    #         'context': {
-    #             'default_patient_id': self.id,  # Set default patient when creating new documents
-    #         },
-    #     }
-
-    # def action_open_documents(self):
-    #     self.ensure_one()
-    #     current_visit = self.visit_ids.filtered(lambda v: v.state == 'check_in')
-    #     domain = [('patient_id', '=', self.id)]  # Always filter by patient
-    #     context = {
-    #         'default_patient_id': self.id,
-    #         # Default folder/visit for new documents if current visit exists
-    #         'default_folder_id': current_visit.folder_id.id if current_visit else False,
-    #         'default_visit_id': current_visit.id if current_visit else False,
-    #     }
-    #     return {
-    #         'type': 'ir.actions.act_window',
-    #         'name': 'Patient Documents',
-    #         'res_model': 'documents.document',
-    #         'view_mode': 'tree,form',
-    #         'domain': domain,
-    #         'context': context,
-    #     }
-
-
-
-
-
-
-
-    # def action_check_out(self):
-    #     self.ensure_one()
-    #     return self.write({'state': 'check_out'})
-    #
-    # def write(self, vals):
-    #     # Capture the state change if present
-    #     res = super(Patient, self).write(vals)
-    #     for record in self:
-    #         # When state changes to check_out and end_date is not already set, capture the write_date as end_date
-    #         if record.state == 'check_out' and not record.end_date:
-    #             record.end_date = record.write_date
-    #     return res
-
-
-
-
-    # def action_check_in(self):
-    #
-    #     self.ensure_one()
-    #
-    #     # Get the formatted date
-    #     now = fields.Datetime.now()
-    #     formatted_date = now.strftime('%Y-%m-%d')
-    #
-    #     # Create a folder in documents module
-    #     parent_folder = self.env['documents.folder'].search([('name', '=', 'Patient Documents')], limit=1)
-    #     if not parent_folder:
-    #         parent_folder = self.env['documents.folder'].create({
-    #             'name': 'Patient Documents',
-    #             'description': 'Root folder for all patient documents',
-    #         })
-    #
-    #     # Create a folder in documents module
-    #     folder_name = f"{self.name}_{self.patient_id}/{formatted_date}"
-    #     folder = self.env['documents.folder'].create({
-    #         'name': folder_name,
-    #         'description': f"Visit folder for patient {self.name} with ID {self.patient_id} on {formatted_date}",
-    #         'parent_folder_id': parent_folder.id,
-    #     })
-    #
-    #     # Create a new visit record
-    #     new_visit = self.env['bancat.patient.visit'].create({
-    #         'patient_id': self.id,
-    #         'state': 'check_in',
-    #         'approximate_amount': self.approximate_amount,
-    #         'folder_id': folder.id,
-    #         # start_date will be set automatically (default)
-    #     })
-    #
-    #     # Store the folder ID in the visit record
-    #     # First, add a folder_id field to the PatientVisit model
-    #     if hasattr(new_visit, 'folder_id'):
-    #         new_visit.folder_id = folder.id
-    #
-    #     # Update the patient record
-    #     self.write({
-    #         'state': 'check_in',
-    #         'start_date': fields.Datetime.now(),
-    #         'end_date': False,
-    #     })
-    #     return True
-
-
-
-
-
-
-
-    # @api.model
-    # def create(self, vals):
-    #     # Create a folder for the patient
-    #     patient = super(Patient, self).create(vals)
-    #     folder = self.env['documents.folder'].create({
-    #         'name': patient.name,
-    #         'description': f"Folder for patient {patient.name}",
-    #     })
-    #     patient.document_ids.write({'folder_id': folder.id})
-    #     return patient
